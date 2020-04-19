7c8e4976f11a806f281d96cf9651df0ede94d524
91741 <91741@MOHANREDDY>
2020-04-19 16:36:07 +0530

    Password encoding logic added

--- HomeLedger/src/main/java/org/home/ledger/config/AppSecurityConfig.java
+++ HomeLedger/src/main/java/org/home/ledger/config/AppSecurityConfig.java
@@ -5,25 +5,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Bean
+    public PasswordEncoder passwordEncoderBean() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoderBean());
+	}
 	@Bean
 	public AuthenticationProvider authProvider() {
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		provider.setUserDetailsService(userDetailsService);
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder());
 		return provider;
 	}
 	
--- HomeLedger/src/main/java/org/home/ledger/controller/UserController.java
+++ HomeLedger/src/main/java/org/home/ledger/controller/UserController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.home.ledger.model.User;
 import org.home.ledger.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,12 +16,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@PostMapping("/register")
 	public String userRegistration(HttpServletRequest request, @ModelAttribute("User") User user) {
 		request.setAttribute("MODE", "REGISTER");
 		List<User> user1 = userService.checkUser(user.getEmailId(),user.getMobileNumber());
 		if(user1 != null && !user1.isEmpty()) {
-			System.out.println(user1);
 			request.setAttribute("status", "failed");
 			request.setAttribute("user", user);
 			return "welcome";
@@ -28,6 +31,8 @@ public class UserController {
 		else{
 			request.setAttribute("MODE", "LOGIN");
 			request.setAttribute("status", "register-success");
+			String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+			user.setPassword(encodedPassword);
 			userService.registerUser(user);
 			return "welcome";
 		}
